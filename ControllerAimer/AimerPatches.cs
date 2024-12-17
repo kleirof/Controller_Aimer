ff1995848059d46747049049f85fb50ef16852c0
kleirof <95400614+kleirof@users.noreply.github.com>
2024-12-17 19:41:37 +0800

    Add files via upload

---
+++ ControllerAimer/AimerPatches.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using HarmonyLib;
+using MonoMod.Cil;
+using Mono.Cecil.Cil;
+using static MonoMod.Cil.RuntimeILReferenceBag.FastDelegateInvokers;
+using System;
+using System.Reflection;
+
+namespace ControllerAimer
+{
+    public static class AimerPatches
+    {
+        public static void EmitCall<T>(this ILCursor iLCursor, string methodName, Type[] parameters = null, Type[] generics = null)
+        {
+            MethodInfo methodInfo = AccessTools.Method(typeof(T), methodName, parameters, generics);
+            iLCursor.Emit(OpCodes.Call, methodInfo);
+        }
+
+        [HarmonyPatch(typeof(GuidedBulletsPassiveItem), nameof(GuidedBulletsPassiveItem.PreMoveProjectileModifier))]
+        public class PreMoveProjectileModifierPatchClass
+        {
+            [HarmonyILManipulator]
+            public static void PreMoveProjectileModifierPatch(ILContext ctx)
+            {
+                ILCursor crs = new ILCursor(ctx);
+
+                if (crs.TryGotoNext(MoveType.Before,
+                    x => x.MatchStloc(2)))
+                {
+                    crs.Emit(OpCodes.Ldarg_0);
+                    crs.Emit(OpCodes.Ldarg_1);
+                    crs.Emit(OpCodes.Ldloc_0);
+                    crs.Emit(OpCodes.Ldloc_1);
+                    crs.EmitCall<PreMoveProjectileModifierPatchClass>(nameof(PreMoveProjectileModifierPatchClass.PreMoveProjectileModifierPatchCall));
+                }
+            }
+
+            private static float PreMoveProjectileModifierPatchCall(float target, GuidedBulletsPassiveItem self, Projectile p, BraveInput instanceForPlayer, Vector2 vector)
+            {
+                if (!(instanceForPlayer.IsKeyboardAndMouse(false)
+                            || instanceForPlayer.ActiveActions == null)
+                            && vector == Vector2.zero)
+                {
+                    AIActor nearestEnemy = self.Owner.CurrentRoom.GetNearestEnemy(self.Owner.CenterPosition, out _, true, false);
+                    if (nearestEnemy)
+                    {
+                        vector = nearestEnemy.CenterPosition - p.specRigidbody.UnitCenter;
+                        return vector.ToAngle();
+                    }
+                    else
+                        return self.Owner.m_currentGunAngle;
+                }
+                else
+                    return target;
+            }
+        }
+
+        [HarmonyPatch(typeof(InputGuidedProjectile), nameof(InputGuidedProjectile.Move))]
+        public class MovePatchClass
+        {
+            [HarmonyILManipulator]
+            public static void MovePatch(ILContext ctx)
+            {
+                ILCursor crs = new ILCursor(ctx);
+
+                if (crs.TryGotoNext(MoveType.Before,
+                    x => x.MatchStloc(3)))
+                {
+                    crs.Emit(OpCodes.Ldarg_0);
+                    crs.Emit(OpCodes.Ldloc_1);
+                    crs.Emit(OpCodes.Ldloc_2);
+                    crs.EmitCall<MovePatchClass>(nameof(MovePatchClass.MovePatchCall));
+                }
+            }
+
+            private static float MovePatchCall(float target, InputGuidedProjectile self, BraveInput instanceForPlayer, Vector2 vector)
+            {
+                if (!(instanceForPlayer.IsKeyboardAndMouse(false)
+                        || instanceForPlayer.ActiveActions == null)
+                        && vector == Vector2.zero)
+                {
+                    AIActor nearestEnemy = (self.Owner as PlayerController).CurrentRoom.GetNearestEnemy(self.Owner.CenterPosition, out _, true, false);
+                    if (nearestEnemy)
+                    {
+                        vector = nearestEnemy.CenterPosition - self.specRigidbody.UnitCenter;
+                        return vector.ToAngle();
+                    }
+                    else
+                        return (self.Owner as PlayerController).m_currentGunAngle;
+                }
+                else
+                    return target;
+            }
+        }
+    }
+}
---
+++ ControllerAimer/ControllerAimer.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace ControllerAimer
+{
+    [BepInDependency("etgmodding.etg.mtgapi")]
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class AimerModule : BaseUnityPlugin
+    {
+        public const string GUID = "kleirof.etg.controlleraimer";
+        public const string NAME = "Controller Aimer";
+        public const string VERSION = "1.1.3";
+        public const string TEXT_COLOR = "#FF7F50";
+
+		public void Start()
+        {
+            ETGModMainBehaviour.WaitForGameManagerStart(GMStart);
+        }
+
+        public void GMStart(GameManager g)
+        {
+            Log($"{NAME} v{VERSION} started successfully.", TEXT_COLOR);
+
+			Harmony harmony = new Harmony(GUID);
+			harmony.PatchAll();
+		}
+
+        public static void Log(string text, string color = "FFFFFF")
+        {
+            ETGModConsole.Log($"<color={color}>{text}</color>");
+        }
+	}
+}
